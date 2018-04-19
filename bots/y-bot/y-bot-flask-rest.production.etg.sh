238d3571a638099b213d49a46adc13aed64b87eb
Ed Guy <edguy@eguy.org>
2018-04-19 10:51:20 -0700

    add ico details.

--- bots/y-bot/y-bot-flask-rest.production.etg.sh
+++ bots/y-bot/y-bot-flask-rest.production.etg.sh
@@ -5,6 +5,7 @@ pwd
 
 #clear
 
+touch /tmp/y-bot.log
 PYTHONPATH=../../src:. python3 ../../src/programy/clients/flaskrest.py --config ./config.yaml --cformat yaml --logging ./logging.yaml &
 tail -f /tmp/y-bot.log
 
