0653ab4920b596a99040aeca7c58231705677adb
edguy <edguy@eguy.org>
2018-05-18 00:35:04 -0400

    pre merge

--- bots/y-bot/y-bot-flask-rest.production.etg.sh
+++ bots/y-bot/y-bot-flask-rest.production.etg.sh
@@ -5,8 +5,8 @@ pwd
 
 #clear
 
-PYTHONPATH=../../src:. python3 ../../src/programy/clients/flaskrest.py --config ./config.yaml --cformat yaml --logging ./logging.yaml &
-tail -f /tmp/y-bot.log
+PYTHONPATH=../../src:. python3 ../../src/programy/clients/flaskrest.py --config ./config.yaml --cformat yaml --logging ./logging.yaml 
+#tail -f /tmp/y-bot.log
 
 
 
