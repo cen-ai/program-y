e1a30d70e9dee4ba9c218ae1645f013e9e7445f5
Ed Guy <edguy@eguy.org>
2018-07-09 00:16:57 -0400

    prune files

---
+++ bots/cen/y-bot-flask-rest.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+CLS
+
+mkdir .\temp
+
+SET PYTHONPATH=..\..\src;.
+
+python ..\..\src\programy\clients\flaskrest.py --config .\config.windows.yaml --cformat yaml --logging .\logging.windows.yaml
+
--- etg.run.sh
+++ etg.run.sh
@@ -1,6 +1,14 @@
 
 echo apt-get install libxml2-dev libxslt-dev
 echo pip3 install -r requirements.txt 
-screen bots/y-bot/y-bot-flask-rest.production.etg.sh
+screen bots/cen/cen-flask-rest.production.etg.sh
 
 echo curl 'http://localhost:8989/api/v1.0/ask?question=hello+world&sessionid=1234567890'
+
+
+pm2 start /home/edguy/program-y/bots/cen/cen-flask-rest.production.etg.sh
+
+or 
+cd ~edguy/program-y/bots/cen/ ; pm2 start cen-flask-rest.production.etg.sh 
+
+
