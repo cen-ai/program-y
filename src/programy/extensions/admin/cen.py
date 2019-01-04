0f4d82034f62554d03e3ed30a86bd7ab85d56ff3
nitin panuganti <nitin.noornook@gmail.com>
2019-01-04 21:16:47 +0530

    cen extension

---
+++ src/programy/extensions/admin/cen.py
@@ -0,0 +1,59 @@
+"""
+Copyright (c) 2016-2018 Keith Sterling http://www.keithsterling.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions
+of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+from programy.utils.logging.ylogger import YLogger
+
+from programy.extensions.base import Extension
+from programy.storage.factory import StorageFactory
+
+class CenAdminExtension(Extension):
+
+
+    def update_aiml(self, client_context):
+        YLogger.debug(client_context, "Hot reloading all AIML files")
+        client_context.brain.update_aiml()
+        return 'CENADMIN OK'
+
+    # execute() is the interface that is called from the <extension> tag in the AIML
+    def execute(self, client_context, data):
+        print("YODA2")
+        YLogger.debug(client_context, "Cen Admin - [%s]", data)
+
+        try:
+            commands = CenAdminExtension.split_into_commands(data)
+
+            if commands[0] == 'UPDATE':
+
+                entity = commands[1]
+
+                if entity in ['AIML']:
+                    if entity == 'AIML':
+                            return self.update_aiml(client_context)
+                else:
+                    raise Exception ("Unknown reload entity [%s]"%entity)
+
+            elif commands[0] == 'COMMANDS':
+                return "UPDATE [AIML]"
+
+            else:
+                raise Exception ("Unknown reload command [%s]"%commands[0])
+
+        except Exception as e:
+            print(e)
+            YLogger.exception(client_context, "Failed to execute cen extension", e)
+
+        return "Cen Admin Error"
